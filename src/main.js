8a11a165eed6d2530170c11681e212e2fa3583e0
reminderclock <reminderclock@naver.com>
2020-12-09 23:52:26 +0900

    Update frist cube in js [step-2]
    Updated prototype cube

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
 
-
-
+// make frist cube
 function makeDefalutCube() {
-    let cubeInfo = [
-    {line: "top", info: ["R", "R", "W"]},
-    {line: "center", info: ["G", "C", "W"]},
-    {line: "bottom", info: ["G", "B", "B"]},
-    {line: "right", info: ["W", "W", "B"]},
-    {line: "left", info: ["R", "G", "G"]},
-    ];
+    let left = ['R', 'G', 'G'];
+    let right = ['W', 'W', 'B'];
+    let top = [left[0], 'R', right[0]];
+    let center = [left[1], 'C', right[1]];
+    let bottom = [left[2], 'B', right[2]];
+    let cubeInfo = [top, center, bottom];
     let cube = '';
-    const defaultArr = cubeInfo.map( a=> a.info).slice(0,3);
-        defaultArr.forEach( (arr) => {
+        cubeInfo.forEach( (arr) => {
         cube += arr.join(' ') + '</br>';
     });
     displayCube(cube);
 }
 
+// display cube
 function displayCube(cube) {
     const container = document.querySelector('.cube');
     container.innerHTML = `<p>${cube}</p>`;
