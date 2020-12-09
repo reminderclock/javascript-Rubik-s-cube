96d62f299ef4b6fe5c8782f2a69a1bf0fc3f6048
reminderclock <reminderclock@naver.com>
2020-12-09 19:25:05 +0900

    Split the function in detail
    Added decideShift, shiftLeft&Right function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 // limit parameters range
 function limitRange() {
     let info = prompt("공백으로 구분하여 단어 하나, 정수 숫자 하나( -100 <= N < 100) , L 또는 R을(대소문자구분❌) 입력하세요. ex)apple 3 L").split(' ');
@@ -14,26 +13,39 @@ function limitRange() {
     if(direction.toUpperCase()!=="L" && direction.toUpperCase()!=="R"){
         return alert("입력 형식에 벗어납니다.");
     }
-    return pushWord(str, num, direction.toUpperCase());
+    return decideShift(info);
 }
 
-
-
-// push word
-function pushWord(str, num, direction) {
-    const word = str.split('');
-    if((direction==='L' && Math.sign(num)===1) || (direction==='R' && Math.sign(num)===-1)) {
-        for(let i=0; i<Math.abs(num); i++) {
-            word.push(word[i]);
-        }
-        word.splice(0, Math.abs(num));       
+// decide direction
+function decideShift(info) {
+    let shift = info[2].toUpperCase();
+    if((shift==='L' && Math.sign(info[1])===1) || (shift==='R' && Math.sign(info[1])===-1)) {
+        shiftLeft(info);
     }
-    else if((direction==='L' && Math.sign(num)===-1) || (direction==='R' && Math.sign(num)===1)) {
-        for(let j=0; j<Math.abs(num); j++) {
-            word.unshift(word[word.length-1-j]);
-        }
-        word.splice(word.length-Math.abs(num));        
+    else{
+        shiftRight(info);
     }
+}
+
+// Case: shift left
+function shiftLeft(info) {
+    let word = info[0].split('');
+    let num = info[1];
+    for(let i=0; i<Math.abs(num); i++) {
+    word.push(word[i]);
+    }
+    word.splice(0, Math.abs(num));
+    displayWord(word.join(''));
+}
+
+// Case: shift right
+function shiftRight(info) {
+    let word = info[0].split('');
+    let num = info[1];
+    for(let j=0; j<Math.abs(num); j++) {
+    word.unshift(word[word.length-1-j]);
+    }
+    word.splice(word.length-Math.abs(num));        
     displayWord(word.join(''));
 }
 
