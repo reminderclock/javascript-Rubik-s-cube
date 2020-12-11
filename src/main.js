5f19294a190e3f6b80efd3457016b8c9698eb796
reminderclock <reminderclock@naver.com>
2020-12-11 23:22:58 +0900

    Update frist cube in js [step-2]
    Updated Object style

--- src/main.js
+++ src/main.js
@@ -1,56 +1,31 @@
+let cube = {};
+cube.left = ["R", "G", "G"];
+cube.right = ["W", "W", "B"];
+cube.top=[];
+cube.bottom =[];
+cube.center = [];
+cube.fristInfo = [];
+cube.proto='';
+cube.input = '';
+cube.rightInfo = ["U'","R'","L","B"];
+cube.leftInfo = ["U","R","L'","B'"];
+cube.newCube = '';
 
-// make frist cube
-function makeDefalutCube() {
-    let left = ['R', 'G', 'G'];
-    let right = ['W', 'W', 'B'];
-    let top = [left[0], 'R', right[0]];
-    let center = [left[1], 'C', right[1]];
-    let bottom = [left[2], 'B', right[2]];
-    let cubeInfo = [top, center, bottom];
-    let cube = '';
-        cubeInfo.forEach( (arr) => {
-        cube += arr.join(' ') + '\n';
+cube.makeFrist = function() {
+    this.fristInfo=[this.top, this.center, this.bottom];
+    this.frist();
+}
+cube.makeOthers = function() {
+    this.top=[this.left[0], "R", this.right[0]];
+    this.center = [this.left[1], "C", this.right[1]];
+    this.bottom=[this.left[2], "B", this.right[2]];
+    this.makeFrist();
+}
+cube.frist = function() {
+    this.fristInfo.forEach( (arr) => {
+        this.proto += arr.join(' ') + '\n';
     });
-    console.log(cube);
-    displayPrompt(cubeInfo);
+    console.log(this.proto);
 }
 
-// Add prompt
-function displayPrompt(cube) {
-    let input = prompt('cube>');
-    decideShift(cube, input);
-}
-
-// decideShift test
-function decideShift(cube, input) {
-    let right = ["U'","R'","L","B"];
-    let left = ["U","R","L'","B'"];
-    let bye = "Bye~";
-    if(right.includes(input)===true) {
-        let newCube = '';
-        newCube += input;
-        cube.forEach( (arr) => {
-        newCube += '\n' + arr.join(' ');
-    });
-        shiftRight(cube);
-    }
-    else if(left.includes(input)===true) {
-        let newCube = '';
-        newCube += input;
-        cube.forEach( (arr) => {
-        newCube += '\n' + arr.join(' ');
-    });
-        shiftLeft(cube);
-    }
-    else if(input==="Q") {
-        console.log(bye);
-    }
-    }
-    
-    // function shiftRight(cube) {
-    
-    // }
-    
-    
-    
-    makeDefalutCube();
+cube.makeOthers();
